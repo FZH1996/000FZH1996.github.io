96cd6d3b7744906637f3fdad36422bfde0920144
FZH1996 <3291976780@qq.com>
2022-08-15 01:52:07 +0800

    publish site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220815.014620';
+const cacheName = 'chirpy-20220815.015158';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
